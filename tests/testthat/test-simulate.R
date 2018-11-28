8bfe26f0f05adf732f970b6c9c67f5d75bcc43f1
Ivan Ukhov <ivan.ukhov@leovegas.com>
2018-11-28 09:02:32 +0100

    Add a test

--- tests/testthat/test-simulate.R
+++ tests/testthat/test-simulate.R
@@ -1,5 +1,15 @@
 context('simulate')
 
+test_that('the number of rows is correct', {
+  set.seed(0)
+  data <- tibble(replication = seq_len(42)) %>%
+    simulate(day_num = 7,
+             daily_num = 10,
+             a_rate = 0.01,
+             b_effect = 0)
+  expect_equal(nrow(data), 42 * 7)
+})
+
 test_that('the expected loss is correct', {
   set.seed(0)
   data <- tibble(replication = seq_len(20)) %>%
@@ -12,7 +22,6 @@ test_that('the expected loss is correct', {
              a_beta_prior = 990,
              b_beta_prior = 990,
              expected_loss = TRUE)
-  expect_equal(nrow(data), 20 * 14)
   expect_true(abs(sum(data$expected_loss) - 0.1529044) < 1e-6)
 })
 
@@ -29,6 +38,5 @@ test_that('the approximate expected loss is correct', {
              b_beta_prior = 90,
              expected_loss = TRUE,
              approximate = TRUE)
-  expect_equal(nrow(data), 10 * 7)
   expect_true(abs(sum(data$expected_loss) - 0.01946671) < 1e-6)
 })
