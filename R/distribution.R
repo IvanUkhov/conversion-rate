c0abecc6a6d686f630664465aaac4ab599afa557
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-12-04 17:46:58 +0100

    Fix the effect density for narrow distributions

--- R/distribution.R
+++ R/distribution.R
@@ -2,9 +2,18 @@ effect_density_function <- function(alpha_a, beta_a,
                                     alpha_b, beta_b,
                                     convolution = c(0, 1),
                                     tolerance = 1e-8) {
+  if (length(convolution) == 1) {
+    mean <- alpha_b / (alpha_b + beta_b)
+    variance <- alpha_b * beta_b / (alpha_b + beta_b)^2 / (alpha_b + beta_b + 1)
+    convolution <- c(mean - convolution * sqrt(variance),
+                     mean + convolution * sqrt(variance))
+  }
   density_a <- function(x) dbeta(-x, alpha_a, beta_a)
   density_b <- function(x) dbeta(x, alpha_b, beta_b)
   evaluate <- function(y, x) density_b(y) * density_a(x - y)
-  function(x) integrate(evaluate, convolution[1], convolution[2], x,
+  function(x) integrate(evaluate,
+                        max(0, convolution[1]),
+                        min(1, convolution[2]),
+                        x,
                         abs.tol = tolerance)$value
 }
--- tests/testthat/test-distribution.R
+++ tests/testthat/test-distribution.R
@@ -1,11 +1,27 @@
 context('distribution')
 
 test_that('the effect density is correct', {
-  compute <- effect_density_function(alpha_a = 1, beta_a = 1, alpha_b = 1, beta_b = 1)
+  compute <- effect_density_function(alpha_a = 1, beta_a = 1,
+                                     alpha_b = 1, beta_b = 1)
   compute <- Vectorize(compute)
   tibble(effect = c(-2, -1, -0.5, 0, 0.5, 1, 2),
          expected = c(0, 0, 0.5, 1, 0.5, 0, 0)) %>%
-    mutate(computed = compute(effect)) %>%
-    summarize(error = max(abs(computed - expected))) %>%
-    { expect_true(.$error < 1e-10) }
+    mutate(computed = compute(effect),
+           error = abs(computed - expected)) %>%
+    { expect_true(max(.$error) < 1e-10) }
+})
+
+test_that('the effect density is correct for narrow distributions', {
+  set.seed(0)
+  compute <- effect_density_function(alpha_a = 1200, beta_a = 64873,
+                                     alpha_b = 1065, beta_b = 64829,
+                                     convolution = 3)
+  compute_empirical <- approxfun(density(rbeta(1000000, 1065, 64829) -
+                                         rbeta(1000000, 1200, 64873)))
+  compute <- Vectorize(compute)
+  tibble(effect = c(-0.003, -0.002, -0.001)) %>%
+    mutate(computed = compute(effect),
+           expected = compute_empirical(effect),
+           error = abs(computed - expected) / expected) %>%
+    { expect_true(max(.$error) < 1e-2) }
 })
